0ca6b12b40346b959bd9aa97271756ebb6405e42
gabkhanfig <gabkhanfig@gmail.com>
2023-10-14 04:59:22 -0400

    Testing Knobs

--- lowstepper/src/TestLowstepper.cpp
+++ lowstepper/src/TestLowstepper.cpp
@@ -3,6 +3,7 @@
 
 LowStepperOutput lastOutputA; // Default constructed to all fields as 0
 LowStepperLfo* lfoA = nullptr;
+float phase = 0.f;
 
 struct TestLowstepper : Module {
 	enum ParamId {
@@ -33,7 +34,37 @@ struct TestLowstepper : Module {
 		if(lfoA == nullptr) {
 			INFO("Initializing lowstepper with sample rate of %fhz", args.sampleRate);
 			lfoA = new LowStepperLfo(args.sampleRate);
+		}		const float morphPot = getInput(PITCH_INPUT).getVoltage();
+
+		// Below here is all temporary.
+
+		// When cv is not plugged in, it has a default value of 0.5. Value must be 
+		float morphCv = getInput(PITCH_INPUT).isConnected() ? getInput(PITCH_INPUT).getVoltage() : 0.5;
+		if(morphCv > 1 || morphCv < 0) {
+			INFO("morph cv is out of range %f", morphCv);
+			morphCv = 0.5;
 		}
+
+		LowStepperInput inputA;
+		inputA.phase = lastOutputA.phase;
+		inputA.frequency = LowStepperLfo::mapRateInputToFrequency(1, false, LowStepperLfoMode::MEDIUM, 120);
+		inputA.morph = LowStepperLfo::mapMorphInput(combinePotAndCv(morphPot, morphCv));
+		inputA.start = 0;
+		inputA.end = 0;
+		inputA.shouldReset = false;
+
+		lastOutputA = lfoA->tick(inputA);
+		
+		float freq = dsp::FREQ_C4 * std::pow(2.f, inputA.morph);
+
+		// Accumulate the phase
+		phase += freq * args.sampleTime / 4.f;
+		if (phase >= 1.f)
+			phase -= 1.f;
+
+		// Compute the sine output
+		float sine = std::sin(2.f * M_PI * phase);
+		getOutput(SINE_OUTPUT).setVoltage(5.f * sine);
 	}
 };
 
