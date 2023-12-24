2dafec192c00e62cfa0265e7e1767f152b7485ab
Rafael Khan <raf.khan.f@gmail.com>
2023-12-24 02:31:39 -0500

    discomfort module setup

---
+++ OkaySynthesizer/src/discomfort.cpp
@@ -0,0 +1,55 @@
+#include "plugin.hpp"
+
+
+struct Discomfort : Module {
+	enum ParamId {
+		PITCH_PARAM,
+		PARAMS_LEN
+	};
+	enum InputId {
+		PITCH_INPUT,
+		INPUTS_LEN
+	};
+	enum OutputId {
+		SINE_OUTPUT,
+		OUTPUTS_LEN
+	};
+	enum LightId {
+		BLINK_LIGHT,
+		LIGHTS_LEN
+	};
+
+	Discomfort() {
+		config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
+		configParam(PITCH_PARAM, 0.f, 1.f, 0.f, "");
+		configInput(PITCH_INPUT, "");
+		configOutput(SINE_OUTPUT, "");
+	}
+
+	void process(const ProcessArgs& args) override {
+	}
+};
+
+
+struct DiscomfortWidget : ModuleWidget {
+	DiscomfortWidget(Discomfort* module) {
+		setModule(module);
+		setPanel(createPanel(asset::plugin(pluginInstance, "res/discomfort.svg")));
+
+		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
+		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+
+		addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(15.24, 46.063)), module, Discomfort::PITCH_PARAM));
+
+		addInput(createInputCentered<PJ301MPort>(mm2px(Vec(15.24, 77.478)), module, Discomfort::PITCH_INPUT));
+
+		addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(15.24, 108.713)), module, Discomfort::SINE_OUTPUT));
+
+		addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(15.24, 25.81)), module, Discomfort::BLINK_LIGHT));
+	}
+};
+
+
+Model* modelDiscomfort = createModel<Discomfort, DiscomfortWidget>("discomfort");
