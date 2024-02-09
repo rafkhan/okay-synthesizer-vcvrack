ae2ca1857dcf5fe433eddad47c47975716f3e1f2
gabkhanfig <40899981+gabkhanfig@users.noreply.github.com>
2024-02-09 03:35:59 -0500

    Alpha Version Of Lowstepper (#10)

--- lowstepper/src/plugin.cpp
+++ lowstepper/src/plugin.cpp
@@ -7,8 +7,8 @@ Plugin* pluginInstance;
 void init(Plugin* p) {
 	pluginInstance = p;
 	
-	p->addModel(modelDebugLowstepperV7);
-	INFO("loaded debug lowstepper v7");
+	p->addModel(modelLowstepper);
+	INFO("loaded lowstepper");
 
 	// Any other plugin initialization may go here.
 	// As an alternative, consider lazy-loading assets and lookup tables when your module is created to reduce startup times of Rack.
