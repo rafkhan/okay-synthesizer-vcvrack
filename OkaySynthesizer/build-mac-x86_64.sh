269b296d47bcfa1c9e24fed7025b118f47ea23c8
Rafael Khan <raf.khan.f@gmail.com>
2025-05-15 02:53:04 -0400

    update plugin.json

--- OkaySynthesizer/build-mac-x86_64.sh
+++ OkaySynthesizer/build-mac-x86_64.sh
@@ -1,5 +1,7 @@
+export ARCH=x86_64
 export ARCHFLAGS="-arch x86_64"
 export MACOSX_DEPLOYMENT_TARGET=10.9
+export CROSS_COMPILE=x86_64-apple-darwin
 make clean
 rm -r build
-make install CROSS_COMPILE=x86_64-apple-darwin ARCH=x86_64 ARCHFLAGS="-arch x86_64"
+make install CROSS_COMPILE=$CROSS_COMPILE ARCH=$ARCH ARCHFLAGS="$ARCHFLAGS" 
